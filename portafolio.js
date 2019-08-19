24cd63dbb29b6369b95d740f141c2d9bf16b181d
LuBronJames <53785685+LuBronJames@users.noreply.github.com>
2019-08-19 10:02:03 -0400

    Update portafolio.js

--- portafolio.js
+++ portafolio.js
@@ -1,38 +1,38 @@
 var trabajos = [{
-    figure: "media/trabajo-01-desfile.jpg",
+    figure: "P1.png",
     alt: "Descripción de la imagen",
-    title: "Título de primer trabajo",
-    about: "Texto simulado de descripción de primer trabajo"
+    title: "Wako",
+    about: "Proyecto de emergencia textil para Taller: Producto, 2018"
 }, {
-    figure: "media/trabajo-02-app.jpg",
+    figure: "P2.png",
     alt: "Descripción de la imagen",
-    title: "Título de segundo trabajo",
-    about: "Texto simulado de descripción de segundo trabajo"
+    title: "La Tribu",
+    about: "Juego de mesa para fines investigativos para Taller: Producto, 2018"
 }, {
-    figure: "media/trabajo-03-homy.jpg",
+    figure: "P3.png",
     alt: "Descripción de la imagen",
-    title: "Título de tercer trabajo",
-    about: "Texto simulado de descripción de tercer trabajo"
+    title: "Móviles de Magia y Cartón: Rola el auto",
+    about: "Nueva línea de juguetes de medios de transporte para el trabajo con un emprendimiento. TIDE, 2019"
 }, {
-    figure: "media/trabajo-04-editorial.jpg",
+    figure: "P4.png",
     alt: "Descripción de la imagen",
-    title: "Título de cuarto trabajo",
-    about: "Texto simulado de descripción de cuarto trabajo"
+    title: "Tanin",
+    about: "Producto alimenticio a base de mate creado para la feria gastronómica en Taller: Producto, 2018"
 }, {
-    figure: "media/trabajo-05-artesania.jpg",
+    figure: "P5.png",
     alt: "Descripción de la imagen",
-    title: "Título de quinto trabajo",
-    about: "Texto simulado de descripción de quinto trabajo"
+    title: "Esplat",
+    about: "Prensador de alimentos para Ilko. TIDE, 2019"
 }, {
-    figure: "media/trabajo-06-textil.jpg",
+    figure: "P6.png",
     alt: "Descripción de la imagen",
-    title: "Título de sexto trabajo",
-    about: "Texto simulado de descripción de sexto trabajo"
+    title: "Hotel Papiofornia",
+    about: "Dispositivo de enriquecimiento ambiental para papiones. Taller: Identidad, 2017"
 }, {
-    figure: "media/trabajo-07-materiales.jpg",
+    figure: "P9.png",
     alt: "Descripción de la imagen",
-    title: "Título de séptimo trabajo",
-    about: "Texto simulado de descripción de séptimo trabajo"
+    title: "Alcor",
+    about: "Exhibidor de vinos. Taller: Calidad II, 2019"
 }];
 // LUEGO CREAMOS PARTES DEL SITIO CON CADA DATO CONTENIDO EN LA VARIABLE
 $.each(trabajos, function(i, val) {
